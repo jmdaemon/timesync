524024db470e2ef7ac868df117b4cca048912157
Joseph Diza <josephm.diza@gmail.com>
2022-01-16 14:35:38 -0800

    Add calendar days

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
+//use sixtyfps::{Model};
+//use sixtyfps::{Model, ModelHandle, Timer, VecModel};
+use sixtyfps::{Model, ModelHandle, VecModel};
+use std::rc::Rc;
+//use std::time::Duration;
+
+//sixtyfps::include_modules!();
 sixtyfps::include_modules!();
+
+//sixtyfps::sixtyfps! {
+    //import { Days, AppWindow } from "src/ui/calendar.60";
+//}
 
 fn main() {
     let ui = AppWindow::new();
+
+    //let mut tiles: Vec<DayData> = ui.get_days().iter().collect();
+
+    let mut tiles: Vec<DayData> = ui.get_days().iter().collect();
+    //let mut tiles: Vec<DayData> = ui.get_days().into_iter().collect();
+    // Duplicate them to ensure that we have pairs
+    tiles.extend(tiles.clone());
+
+    let tiles_model = Rc::new(VecModel::from(tiles));
+    ui.set_days(ModelHandle::new(tiles_model.clone()));
+    let _appwin_weak = ui.as_weak();
     ui.run();
 }
