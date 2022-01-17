b9e1c1317eb634a95cf38097d6255cdb253f7fe4
Joseph Diza <josephm.diza@gmail.com>
2022-01-17 00:33:00 -0800

    Remove obsolete code

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,40 @@
-use sixtyfps::{Model, ModelHandle, VecModel, SharedString};
-use chrono::{Datelike, Utc, Month};
+use sixtyfps::{ModelHandle, VecModel, SharedString};
+use chrono::{Datelike, Month};
 use num_traits::FromPrimitive;
 use std::rc::Rc;
 
 sixtyfps::include_modules!();
 
-/// Generate days of the month
-fn gen_days(month: &str, ui: &AppWindow) -> Vec<DayData> {
-    let mut days: Vec<DayData> = ui.get_days().iter().collect();
-    for i in 1..31 {
+fn gen_days(numdays: i32) -> Vec<DayData> {
+    let mut days: Vec<DayData> = Vec::new();
+    for i in 1..numdays {
         days.push(DayData {
             daynum: SharedString::from(i.to_string()),
         });
-
     }
     return days;
 }
 
-fn gen_month(month: &str, ui: &AppWindow) -> MonthData {
-    let mut months: MonthData = ui.get_months();
-    months = MonthData {
-        current_month: SharedString::from(month.to_owned())
-    };
-    return months;
-}
+fn gen_month(month: &str) -> MonthData { MonthData { current_month: SharedString::from(month.to_owned()) } }
+fn gen_year(year: &str) -> YearData { YearData { current_year: SharedString::from(year.to_owned()) } }
 
-
-/// Get the current month
-fn month() -> String {
-    let dt = chrono::Utc::now();
-    let month_str = Month::from_u32(dt.month()).unwrap().name().to_owned();
-    return month_str;
-}
-
-fn year() -> i32 {
-    let current_date = chrono::Utc::now();
-    let year = current_date.year();
-    return year;
-}
+fn month() -> String { Month::from_u32(chrono::Utc::now().month()).unwrap().name().to_owned() }
+fn year() -> i32 { chrono::Utc::now().year() }
 
 fn main() {
     let ui = AppWindow::new();
 
-    let months = gen_month(&month(), &ui);
-    let days = gen_days(&month(), &ui);
+    let years = gen_year(&year().to_string());
+    let months = gen_month(&month());
+    let days = gen_days(31);
 
     let days_model = Rc::new(VecModel::from(days));
     let months_model = months;
+    let years_model = years;
 
     ui.set_days(ModelHandle::new(days_model.clone()));
     ui.set_months(months_model);
+    ui.set_years(years_model);
     let _appwin_weak = ui.as_weak();
     ui.run();
 }
