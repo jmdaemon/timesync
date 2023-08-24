72f2966a0d17ee6d231b47994531c8db19e870a9
Joseph Diza <josephm.diza@gmail.com>
2023-08-23 20:31:10 -0700

    Remove unneeded read_file() fn

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,20 @@ use timesync::{
     app::{CLI, Commands, CalendarDisplayType},
     enable_logging,
     calendar::{
-        Event,
-        display_calcomp,
-        parse_events,
         remove_header,
         get_all_events,
         filter_today,
     },
 };
 
-use std::fs::{File, self};
-use std::rc::Rc;
-use std::process::exit;
-use std::io::{self, Read};
+use std::fs;
 
 use anyhow::Result;
 use clap::Parser;
-use icalendar::parser;
 
 #[macro_use]
 #[allow(unused_imports)]
 extern crate tracing;
-
-/// Reads a file into a string and returns the result
-pub fn read_file(path: &str) -> Result<String, io::Error> {
-    let f = File::open(path);
-
-    let mut f = match f {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-
-    let mut s = String::new();
-
-    match f.read_to_string(&mut s) {
-            Ok(_) => Ok(s),
-            Err(err) => Err(err),
-    }
-}
 
 fn main() -> Result<()> {
     let cli = CLI::parse();
